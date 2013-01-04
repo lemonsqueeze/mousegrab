ceccb668a5deb81066864ecdf1efaadd662c4e29
lemonsqueeze <lemonsqueeze@gmx.com>
2013-01-04 18:59:49 +0100

    - removed unused unclutter code & cleanup

--- mousegrab.c
+++ mousegrab.c
@@ -57,29 +57,12 @@ void pexit(str)char *str;{
 void usage(){
     printf(USAGE_MSG);
     exit(1);
-// FIXME: remove unused unclutter code!
-#if 0
-    pexit("usage:\n\
-	-display <display>\n\
-	-idle <seconds>		time between polls to detect idleness.\n\
-	-keystroke		wait for keystroke before idling.\n\
-	-jitter <pixels>	pixels mouse can twitch without moving\n\
-	-grab			use grabpointer method not createwindow\n\
-	-reset			reset the timer whenever cursor becomes\n\
-					visible even if it hasn't moved\n\
- 	-root	       		apply to cursor on root window too\n\
-	-onescreen		apply only to given screen of display\n\
- 	-visible       		ignore visibility events\n\
- 	-noevents      		don't send pseudo events\n\
-	-regex			name or class below is a regular expression\n\
-	-not names...		don't apply to windows whose wm-name begins.\n\
-				(must be last argument)\n\
-	-notname names...	same as -not names...\n\
-	-notclass classes...    don't apply to windows whose wm-class begins.\n\
-				(must be last argument, cannot be used with\n\
-				-not or -notname)");
-#endif
 }
+
+void main_loop(Display *display, Window windowin,
+	       int winx, int winy, int rootx, int rooty);
+void doit(Display *display, Window root, Window *realroot,
+	  Cursor *cursor, int screen, int onescreen, int numscreens);
 
 static void dsleep(float t)
 {
@@ -90,68 +73,8 @@ static void dsleep(float t)
     select(0, NULL, NULL, NULL, &tv);
 }
 
-#define ALMOSTEQUAL(a,b) (abs(a-b)<=jitter)
 #define ANYBUTTON (Button1Mask|Button2Mask|Button3Mask|Button4Mask|Button5Mask)
 
-/* Since the small window we create is a child of the window the pointer is
- * in, it can be destroyed by its adoptive parent.  Hence our destroywindow()
- * can return an error, saying it no longer exists.  Similarly, the parent
- * window can disappear while we are trying to create the child. Trap and
- * ignore these errors.
- */
-int (*defaulthandler)();
-int errorhandler(display,error)
-Display *display;
-XErrorEvent *error;
-{
-    if(error->error_code!=BadWindow)
-	(*defaulthandler)(display,error);
-    return 0;
-}
-
-char **names = 0;	/* -> argv list of names to avoid */
-char **classes = 0;     /* -> argv list of classes to avoid */
-regex_t *nc_re = 0;     /* regex for list of classes/names to avoid */
-
-/*
- * return true if window has a wm_name (class) and the start of it matches
- * one of the given names (classes) to avoid
- */
-int nameinlist(display,window)
-Display *display;
-Window window;
-{
-    char **cpp;
-    char *name = 0;
-
-    if(names)
-	XFetchName (display, window, &name);
-    else if(classes){
-	XClassHint *xch = XAllocClassHint();
-	if(XGetClassHint (display, window, xch))
-	    name = strdup(xch->res_class);
-	if(xch)
-	    XFree(xch);
-    }else
-	return 0;
-
-    if(name){
-	if(nc_re){
-	    if(!regexec(nc_re, name, 0, 0, 0)) {
-		XFree(name);
-		return 1;
-	    }
-	}else{
-	    for(cpp = names!=0 ? names : classes;*cpp!=0;cpp++){
-		if(strncmp(*cpp,name,strlen(*cpp))==0)
-		    break;
-	    }
-	    XFree(name);
-	    return(*cpp!=0);
-	}
-    }
-    return 0;
-}	
 /*
  * create a small 1x1 curssor with all pixels masked out on the given screen.
  */
@@ -188,10 +111,8 @@ Window root;
 int main(int ac, char **av)
 {
     Display *display;
-    int screen,oldx = -99,oldy = -99,numscreens;
-    int doroot = 0, jitter = 0, usegrabmethod = 1, waitagain = 0,
-        dovisible = 1, doevents = 1, onescreen = 0;
-    float idletime = 0.0;
+    int screen,numscreens;
+    int onescreen = 0;
     float delay = 0.0;
     Cursor *cursor;
     Window *realroot;
@@ -219,57 +140,6 @@ int main(int ac, char **av)
 	}
 	else
 	    usage();
-#if 0    	
-	if(strcmp(*av,"-idle")==0){
-	    ac--,av++;
-	    if(ac<0)usage();
-	    idletime = atof(*av);
-	}else if(strcmp(*av,"-keystroke")==0){
-	    idletime = -1;
-	}else if(strcmp(*av,"-jitter")==0){
-	    ac--,av++;
-	    if(ac<0)usage();
-	    jitter = atoi(*av);
-	}else if(strcmp(*av,"-noevents")==0){
-	    doevents = 0;
-	}else if(strcmp(*av,"-root")==0){
-	    doroot = 1;
-	}else if(strcmp(*av,"-grab")==0){
-	    usegrabmethod = 1;
-	}else if(strcmp(*av,"-reset")==0){
-	    waitagain = 1;
-	}else if(strcmp(*av,"-onescreen")==0){
-	    onescreen = 1;
-	}else if(strcmp(*av,"-visible")==0){
-	    dovisible = 0;
-	}else if(strcmp(*av,"-regex")==0){
-	    nc_re = (regex_t *)malloc(sizeof(regex_t));
-	}else if(strcmp(*av,"-not")==0 || strcmp(*av,"-notname")==0){
-	    /* take rest of srg list */
-	    names = ++av;
-	    if(*names==0)names = 0;	/* no args follow */
-	    ac = 0;
-	}else if(strcmp(*av,"-notclass")==0){
-	    /* take rest of arg list */
-	    classes = ++av;
-	    if(*classes==0)classes = 0;	/* no args follow */
-	    ac = 0;
-	}else 
-#endif
-    }
-    /* compile a regex from the first name or class */
-    if(nc_re){
-	if(names || classes){
-	    if (regcomp(nc_re, (names != 0 ? *names : *classes),
-			REG_EXTENDED | REG_NOSUB)) { /* error */
-		free(nc_re);
-		names = classes = 0;
-		nc_re = 0;
-	    }
-	}else{ /* -regex without -not... ... */
-	    free(nc_re);
-	    nc_re = 0;
-	}
     }
 
     display = XOpenDisplay(displayname);
@@ -288,14 +158,10 @@ int main(int ac, char **av)
 	}else{
 	    realroot[screen] = XRootWindow(display,screen);
 	    cursor[screen] = createnullcursor(display,realroot[screen]);
-	    if(idletime<0)
-		XSelectInput(display,realroot[screen],KeyReleaseMask);
 	}
     screen = DefaultScreen(display);
     root = VirtualRootWindow(display,screen);
 
-    if(!usegrabmethod)
-	defaulthandler = XSetErrorHandler(errorhandler);
     /*
      * create a small unmapped window on a screen just so xdm can use
      * it as a handle on which to killclient() us.
@@ -307,196 +173,101 @@ int main(int ac, char **av)
 	dsleep(delay);
     printf("Grabbing mouse. Right click to ungrab.\n");
     
-    while(1){
-	Window dummywin,windowin,topwin,newroot;
-	int rootx,rooty,winx,winy, res;
-	unsigned int modifs;
-	Window lastwindowavoided = None;
-	
-	/*
-	 * wait for pointer to not move and no buttons down
-	 * or if triggered by keystroke check no buttons down
-	 */
-	while(1){
-	    if(idletime<0){		/* wait for keystroke trigger */
-		XEvent event;
-		do{
-		    XNextEvent(display,&event);
-		}while(event.type != KeyRelease ||
-		       (event.xkey.state & ANYBUTTON));
-		oldx = event.xkey.x_root;
-		oldy = event.xkey.y_root;
+    doit(display, root, realroot, cursor, screen, onescreen, numscreens);
+    return 0;
+}
+
+void doit(Display *display, Window root, Window *realroot,
+	  Cursor *cursor, int screen, int onescreen, int numscreens)
+{
+    Window dummywin,windowin,topwin,newroot;
+    int rootx,rooty,winx,winy, res;
+    unsigned int modifs;
+    Window lastwindowavoided = None;
+
+    /* wait for no buttons down */
+    for (; 1; dsleep(0.2))
+    {
+	res = XQueryPointer(display, root, &newroot, &windowin,
+			    &rootx, &rooty, &winx, &winy, &modifs);
+	topwin = windowin;
+	if (!res)
+	{
+	    /* window manager with virtual root may have restarted
+	     * or we have changed screens */
+	    if(!onescreen){
+		for(screen = 0;screen<numscreens;screen++)
+		    if(newroot==realroot[screen])break;
+		if(screen>=numscreens)
+		    pexit("not on a known screen");
 	    }
-	    res = XQueryPointer(display, root, &newroot, &windowin,
-				&rootx, &rooty, &winx, &winy, &modifs);
-	    topwin = windowin;
-	    if (!res)
-	    {
-		/* window manager with virtual root may have restarted
-		 * or we have changed screens */
-		if(!onescreen){
-		    for(screen = 0;screen<numscreens;screen++)
-			if(newroot==realroot[screen])break;
-		    if(screen>=numscreens)
-			pexit("not on a known screen");
-		}
-		root = VirtualRootWindow(display,screen);
-	    }else if((!doroot && windowin==None) || (modifs & ANYBUTTON) ||
-		     !(ALMOSTEQUAL(rootx,oldx) && ALMOSTEQUAL(rooty,oldy))){
-		oldx = rootx, oldy = rooty;
-	    }else if(windowin==None){
-		windowin = root;
-		break;
-	    }else if(windowin!=lastwindowavoided){
-		/* descend tree of windows under cursor to bottommost */
-		Window childin;
-		int toavoid = xFalse;
-		lastwindowavoided = childin = windowin;
-		do{
-		    windowin = childin;
-		    if(nameinlist (display, windowin)){
-			toavoid = xTrue;
-			break;
-		    }
-		}while(XQueryPointer(display, windowin, &dummywin,
-		     &childin, &rootx, &rooty, &winx, &winy, &modifs)
-		       && childin!=None);
-		if(!toavoid){
-		    lastwindowavoided = None;
-		    break;
-		}
-	    }
-	    if(idletime>=0)
-		dsleep(idletime);
+	    root = VirtualRootWindow(display,screen);
+	    continue;
 	}
-	// printf("topwin: %x windowin: %x\n", (int)topwin, (int) windowin);
-	
-	/* wait again next time */
-	if(waitagain)
-	    oldx = -1-jitter;
-	if(usegrabmethod){
-	    if(XGrabPointer(display, root, 0,
-		    PointerMotionMask|ButtonPressMask|ButtonReleaseMask,
-		    GrabModeAsync, GrabModeAsync, None, cursor[screen],
-		    CurrentTime)==GrabSuccess){
-		XEvent event;
-		do{
-		    //Window target = topwin;    // lock on first topwin
-		    Window target = windowin;
-		    //Window target = event.xbutton.subwindow;
-
-		    // TODO: exit if target window gets destroyed...
-		    
-		    XNextEvent(display,&event);
-
-		    // Forward button clicks to initial window
-		    if ((event.type == ButtonPress ||
-			 event.type == ButtonRelease) &&
-			event.xbutton.button != 3)
-		    {
-			event.xbutton.window = target;
-			event.xbutton.subwindow = target;
-			event.xbutton.x = winx;
-			event.xbutton.y = winy;
-			event.xbutton.x_root = rootx;
-			event.xbutton.y_root = rooty;
-			(void)XSendEvent(display, target,
-					 1,
-					 (event.type == ButtonPress ? ButtonPressMask : ButtonReleaseMask),
-					 &event);
-		    }
-
-		}while(!(event.type == ButtonRelease &&
-			     event.xbutton.button == 3));
-		/* keep grabbing the mouse until right click */
-
-		XUngrabPointer(display, CurrentTime);
-		exit(0);
-
-	    }else{
-		/* go to sleep to prevent tight loops */
-		if(idletime>=0)
-			dsleep(idletime);
-	    }
-	}else{
-	    XSetWindowAttributes attributes;
-	    XEvent event;
-	    Window cursorwindow;
-	    
-	    /* create small input-only window under cursor
-	     * as a sub window of the window currently under the cursor
-	     */
-	    attributes.event_mask = LeaveWindowMask |
-			EnterWindowMask |
-			StructureNotifyMask |
-			FocusChangeMask;
-	    if(dovisible)
-		attributes.event_mask |= VisibilityChangeMask;
-	    attributes.override_redirect = True;
-	    attributes.cursor = cursor[screen];
-	    cursorwindow = XCreateWindow
-		(display, windowin,
-		 winx-jitter, winy-jitter,
-		 jitter*2+1, jitter*2+1, 0, CopyFromParent,
-		 InputOnly, CopyFromParent, 
-		 CWOverrideRedirect | CWEventMask | CWCursor,
-		 &attributes);
-	    /* discard old events for previously created windows */
-	    XSync(display,True);
-	    XMapWindow(display,cursorwindow);
-	    /*
-	     * Dont wait for expose/map cos override and inputonly(?).
-	     * Check that created window captured the pointer by looking
-	     * for inevitable EnterNotify event that must follow MapNotify.
-	     * [Bug fix thanks to Charles Hannum <mycroft@ai.mit.edu>]
-	     */
-	    XSync(display,False);
-	    if(!XCheckTypedWindowEvent(display, cursorwindow, EnterNotify,
-				      &event))
-		oldx = -1-jitter;	/* slow down retry */
-	    else{
-		if(doevents){
-		    /*
-		     * send a pseudo EnterNotify event to the parent window
-		     * to try to convince application that we didnt really leave it
-		     */
-		    event.xcrossing.type = EnterNotify;
-		    event.xcrossing.display = display;
-		    event.xcrossing.window = windowin;
-		    event.xcrossing.root = root;
-		    event.xcrossing.subwindow = None;
-		    event.xcrossing.time = CurrentTime;
-		    event.xcrossing.x = winx;
-		    event.xcrossing.y = winy;
-		    event.xcrossing.x_root = rootx;
-		    event.xcrossing.y_root = rooty;
-		    event.xcrossing.mode = NotifyNormal;
-		    event.xcrossing.same_screen = True;
-		    event.xcrossing.focus = True;
-		    event.xcrossing.state = modifs;
-		    (void)XSendEvent(display,windowin,
-				     True/*propagate*/,EnterWindowMask,&event);
-		}
-		/* wait till pointer leaves window */
-		do{
-		    XNextEvent(display,&event);
-		}while(event.type!=LeaveNotify &&
-		       event.type!=FocusOut &&
-		       event.type!=UnmapNotify &&
-		       event.type!=ConfigureNotify &&
-		       event.type!=CirculateNotify &&
-		       event.type!=ReparentNotify &&
-		       event.type!=DestroyNotify &&
-		       (event.type!=VisibilityNotify ||
-			event.xvisibility.state==VisibilityUnobscured)
-		       );
-		/* check if a second unclutter is running cos they thrash */
-		if(event.type==LeaveNotify &&
-		   event.xcrossing.window==cursorwindow &&
-		   event.xcrossing.detail==NotifyInferior)
-		    pexit("someone created a sub-window to my sub-window! giving up");
-	    }
-	    XDestroyWindow(display, cursorwindow);
-	}
+	if (modifs & ANYBUTTON)
+	    continue;
+	break;
     }
+
+    if (windowin==None)
+	windowin = root;
+    else if(windowin!=lastwindowavoided)
+    {
+	/* descend tree of windows under cursor to bottommost */
+	Window childin;
+	int toavoid = xFalse;
+	lastwindowavoided = childin = windowin;
+	do{
+	    windowin = childin;
+	}while(XQueryPointer(display, windowin, &dummywin,
+			     &childin, &rootx, &rooty, &winx, &winy, &modifs)
+	       && childin!=None);
+	if(!toavoid)
+	    lastwindowavoided = None;
+    }
+
+    // printf("topwin: %x windowin: %x\n", (int)topwin, (int) windowin);
+	
+    if (XGrabPointer(display, root, 0,
+		     PointerMotionMask|ButtonPressMask|ButtonReleaseMask,
+		     GrabModeAsync, GrabModeAsync, None, cursor[screen],
+		     CurrentTime) != GrabSuccess)
+	pexit("Couldn't grab pointer.");
+	
+    main_loop(display, windowin, winx, winy, rootx, rooty);    
+}
+
+void main_loop(Display *display, Window windowin,
+	       int winx, int winy, int rootx, int rooty)
+{
+    XEvent event;
+    do{
+	Window target = windowin;
+		    
+	// TODO: exit if target window gets destroyed...
+		    
+	XNextEvent(display,&event);
+
+	// Forward button clicks to initial window
+	if ((event.type == ButtonPress ||
+	     event.type == ButtonRelease) &&
+	    event.xbutton.button != 3)
+	{
+	    event.xbutton.window = target;
+	    event.xbutton.subwindow = target;
+	    event.xbutton.x = winx;
+	    event.xbutton.y = winy;
+	    event.xbutton.x_root = rootx;
+	    event.xbutton.y_root = rooty;
+	    (void)XSendEvent(display, target,
+			     1,
+			     (event.type == ButtonPress ? ButtonPressMask : ButtonReleaseMask),
+			     &event);
+	}
+	
+    /* keep grabbing the mouse until right click */
+    } while (!(event.type == ButtonRelease &&
+	       event.xbutton.button == 3));
+
+    XUngrabPointer(display, CurrentTime);
 }
